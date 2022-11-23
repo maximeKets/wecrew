c12b7d59b3cba697178086887266547ac1d17f24
maximeKets <maximekets80@gmail.com>
2022-11-23 13:35:55 +0100

    feat : add dropdown menu, have to run npm install for load cdn librairy

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,10 +11,46 @@
         <script src="https://use.typekit.net/bez5hou.js" type="text/javascript"></script>
 
         <!-- Scripts -->
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
+        <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-background">
-        <div class=" antialiased">
+    <header>
+
+        <nav class="px-2 sm:px-4 py-2.5 rounded ">
+            <div class="container flex flex-wrap items-center justify-between mx-auto">
+                <a href="{{ route('home') }}"><img src="{{asset('storage/logo.svg')}}" alt="logo"></a>
+                <button data-collapse-toggle="navbar-default" type="button"
+                        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-ok focus:bg-ok "
+                        aria-controls="navbar-default" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                              clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul class="flex flex-col p-4 mt-4  rounded-lg text-xl md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
+                            <li class="flex items-center justify-center">
+                                <a href="{{route('login')}}"
+                                   class="italic block py-2 pl-3 pr-4 text-lg text-gray-700 rounded  md:border-0  md:p-0">sign
+                                    in</a>
+                            </li>
+                            <li class="flex items-center justify-center">
+                                <a href="{{ route('register') }}"
+                                   class="italic block py-2 pl-3 pr-4 bg-ok text-lg text-gray-700 rounded-full border-2  border-black  md:p-3 md:px-6">Join
+                                    the crew</a>
+                            </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+        <div class="antialiased mt-5" >
             {{ $slot }}
         </div>
     </body>
