d5a6ddae3d0e15eefdc0ba19d9e61d22ef98e11b
maximeKets <maximekets80@gmail.com>
2022-11-24 11:48:04 +0100

    fix : load fonts

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,6 +9,9 @@
 
         <!-- Fonts -->
         <script src="https://use.typekit.net/bez5hou.js" type="text/javascript"></script>
+        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+        <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+        <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
 
         <!-- Scripts -->
         <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
